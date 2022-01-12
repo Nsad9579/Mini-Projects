63de2fb64e6c8b7b3e985bc24cae36b97dac4130
Najmeh Sadeghian <53291817+Nsad9579@users.noreply.github.com>
2022-01-12 23:58:13 +0330

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,1 +1,17 @@
-import pandas as pd 
+#STRUCTURE
+#step 1: Connect to kaggle
+#step 2: import datasets
+#step 3: 
+
+
+
+#step 1
+pip install kaggle
+mkdir ~/.kaggle
+cp kaggle.json ~/.kaggle
+! chmod 600 ~/.kaggle/kaggle.json
+! kaggle datasets download uciml/sms-spam-collection-dataset
+! unzip sms-spam-collection-dataset
+! pip install kaggle
+! mkdir ~/.kaggle
+! cp /content/drive/MyDrive/kaggle.json ~/.kaggle/kaggle.json
